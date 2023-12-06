00b280f9d6edc3f5e04b251ab0b39bf260346e1a
Lucas Edson <lucasedson654@gmail.com>
2023-12-05 21:03:24 -0300

    feat: Memory pt1

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,111 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
+use sysinfo::{CpuExt, NetworkExt, ProcessExt, System, SystemExt};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Process {
+    pub pid: u32,
+    pub name: String,
+    pub disk_usage: u64,
+    pub memory_usage: u64,
+    pub cpu_usage: f32,
+    pub path: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Memory {
+    pub total: u64,
+    pub used: u64,
+    pub free: u64,
+    pub available: u64,
+}
+
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Networks {
+    pub name: String,
+    pub mac: String,
+    pub recv: u64,
+    pub send: u64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CpuInformation {
+    pub name: String,
+    pub cpu_usage: f32,
+}
+#[tauri::command]
+fn get_all_informations() -> Value {
+    let mut sys = System::new_all();
+
+    sys.refresh_all();
+
+    fn get_all_processes(sys: &mut System) -> Vec<Process> {
+        let mut processes: Vec<Process> = Vec::new();
+        for (pid, process) in sys.processes() {
+            let new_pid: u32 = format!("{}", pid).parse().unwrap();
+            processes.push(Process {
+                pid: new_pid,
+                name: process.name().to_string(),
+                disk_usage: process.disk_usage().total_read_bytes,
+                memory_usage: process.memory(),
+                cpu_usage: process.cpu_usage(),
+                path: process.exe().to_string_lossy().to_string(),
+            })
+        }
+        return processes;
+    }
+
+    fn get_memory(sys: &mut System) -> Memory {
+        let memory: Memory = Memory {
+            total: sys.total_memory(),
+            used: sys.used_memory(),
+            free: sys.free_memory(),
+            available: sys.available_memory(),
+        };
+        return memory;
+    }
+
+    fn get_networks(sys: &mut System) -> Vec<Networks> {
+        let mut networks: Vec<Networks> = Vec::new();
+        for (name, net_data) in sys.networks() {
+            networks.push(Networks {
+                name: name.to_string(),
+                mac: net_data.mac_address().to_string(),
+                recv: net_data.total_packets_received(),
+                send: net_data.total_packets_transmitted(),
+            })
+        }
+        return networks;
+    }
+
+    fn get_cpu_informations(sys: &mut System) -> Vec<CpuInformation> {
+        let mut cpu_informations: Vec<CpuInformation> = Vec::new();
+        for cpu in sys.cpus() {
+            cpu_informations.push(CpuInformation {
+                name: cpu.name().to_string(),
+                cpu_usage: cpu.cpu_usage(),
+            })
+        }
+        return cpu_informations;
+    }
+
+    let json = json!({
+        "processes": get_all_processes(&mut sys),
+        "memory": get_memory(&mut sys),
+        "networks": get_networks(&mut sys),
+        "cpus" : get_cpu_informations(&mut sys)
+    });
+
+    return json;
+}
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![get_all_informations])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/components/memory/index.jsx
+++ src/components/memory/index.jsx
@@ -1,8 +1,121 @@
+'use client'
+import { useEffect, useState } from 'react';
+import { invoke } from '@tauri-apps/api/tauri'
+import {Chart, ArcElement } from "chart.js";
+import {Doughnut} from 'react-chartjs-2';
+import  './styles.css'
+
+Chart.register(ArcElement)
+
+
 export default function MemoryArea() {
+    const [object, setObject] = useState('');
+    
+    
+    useEffect(() => {
+        setInterval(() =>{
+            
+            invoke('get_all_informations', { name: 'Next.js' })
+            .then(result => setObject(result))
+            .catch(console.error)
+            
+        }, 1000)
+    }, [])
+    
+    let state = {
+          labels: ['Memory', 'Free'],
+      datasets: [
+        {
+          label: '',
+          backgroundColor: [
+              '#F0012C',
+            '#000AF9'
+          ],
+          hoverBackgroundColor: [
+              '#F00011',
+          '#00003f'
+          ],
+
+          borderColor: [
+              '#001f3f',
+          ],
+          data: [(object.memory.used / 1024 / 1024 / 1024).toFixed(2), (object.memory.available / 1024 / 1024 / 1024).toFixed(2)]
+        
+        
+        }
+        
+      ]
+      }
+
     return (
       <main>
-        <div>
-          <p>Memory Area</p>
+        <div className='memorArea'>
+        <div className='memoryChartArea'>
+
+        <Doughnut className='memoryChart'          
+          data={state}
+          options={
+            {
+                cutoutPercentage: 0,
+                circumference: 360,
+                rotation: -90,
+
+                plugins: {
+                    datalabels: {
+                      display: true,
+                      backgroundColor: '#ccc',
+                      borderRadius: 30,
+                      font: {
+                        color: 'red',
+                        weight: 'bold',
+                      },
+                    },
+                    doughnutlabel: {
+                        labels: [
+                          {
+                            text: '550',
+                            font: {
+                              size: 20,
+                              weight: 'bold',
+                            },
+                          },
+                          {
+                            text: 'total',
+                          },
+                        ],
+                      },
+                    
+                  }
+                
+                }
+                
+            }       
+          />
+
+        <div className='totalMemoryInfo'>
+            <h6>Memory</h6>
+            <h1>{
+            (parseFloat((object.memory.used / 1024 / 1024 / 1024)) / 8 * 100).toFixed(0)
+            
+            }%</h1>
+            </div>
+
+        </div>
+
+        <div className='memoryInfo'>
+        <p>Total Memory</p>
+            <h3
+            >{parseFloat(Math.round(object.memory.total / 1024 / 1024 / 1024))} GB</h3>
+
+            <p>Used Memory</p>
+            <h3>{parseFloat((object.memory.used / 1024 / 1024 / 1024).toFixed(2))} GB</h3>
+
+            <p>Free Memory</p>
+            <h3>{parseFloat((object.memory.free / 1024 / 1024 / 1024).toFixed(2))} GB</h3>
+
+            <p>Avaliabe Memory</p>
+            <h3>{parseFloat((object.memory.available / 1024 / 1024 / 1024).toFixed(2))} GB</h3>
+        </div>
         </div>
       </main>
     )
